08dd7021291534d7f2bb9957d438ec186b5bde8d
thanhtranvan <thanh.tran@bluecomgroup.com>
2016-07-21 18:16:55 +0700

    Upload source 2016-07-21

--- app/code/local/Bluecom/Magento1/controllers/CartController.php
+++ app/code/local/Bluecom/Magento1/controllers/CartController.php
@@ -9,6 +9,11 @@ class Bluecom_Magento1_CartController extends Mage_Checkout_CartController
 		$params = $this->getRequest()->getParams();
 		if($params['isAjax'] == 1){
 			$response = array();
+			$response['cart_url'] = Mage::getUrl('checkout/cart');
+			$response['redirect_status'] = Mage::getStoreConfig('bluecom/all_settings/checkout_cart_redirect');
+			$response['redirect_timeout'] = Mage::getStoreConfig('bluecom/all_settings/checkout_cart_redirect_timeout');
+			$response['show_pop_up'] = Mage::getStoreConfig('bluecom/all_settings/show_pop_up_after_add');
+
 			try {
 				if (isset($params['qty'])) {
 					$filter = new Zend_Filter_LocalizedToNormalized(
@@ -17,7 +22,47 @@ class Bluecom_Magento1_CartController extends Mage_Checkout_CartController
 					$params['qty'] = $filter->filter($params['qty']);
 				}
 
-				$product = $this->_initProduct();
+				$session = $this->_getSession();
+	            //if button not contains id in url - load id by url
+	            if(array_key_exists('url', $params)) {
+	                $productUrl = str_replace(Mage::getBaseUrl(), "", $params['url']);
+	                $productId = Mage::getModel('core/url_rewrite')->setStoreId(Mage::app()->getStore()->getId())->loadByRequestPath($productUrl)->getProductId();
+	                $product = Mage::getModel('catalog/product')->load($productId);
+	            } elseif($wishlistItem = (int)$this->getRequest()->getParam('item')) { //if press button add to cart from sidebar wishlist
+	                $item = Mage::getModel('wishlist/item')->load($wishlistItem);
+	                $product = Mage::getModel('catalog/product')->load($item->getProductId());
+
+	                //Add to session wishlist id. It be useful in future if product has options
+	                $session->setWishlistItem($wishlistItem);
+	            } else {
+	                $product = $this->_initProduct();
+	            }
+
+	            /**
+	             *  If product Type_Grouped create another response, but if request from option icon
+	             *  not redirect twice
+	             */
+	            if($product->getTypeInstance(true) instanceof Mage_Catalog_Model_Product_Type_Grouped){
+	                if(!array_key_exists('ajaxAdd', $params)) {
+	                    $url = Mage::getUrl('*/*/options') . 'product_id/' . $product->getEntityId();
+	                    $response['status'] = 'SUCCESS';
+	                    $response['options_url'] = $url;
+	                    $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+	                    return;
+	                }
+	            }
+	            /**
+	             *  If product have options create another response, but if request from option icon
+	             *  not redirect twice
+	             */
+	            if($product->getTypeInstance(true)->hasOptions($product) && !array_key_exists('qty', $params)) {
+	                $url = Mage::getUrl('*/*/options') . 'product_id/' . $product->getEntityId();
+	                $response['status'] = 'SUCCESS';
+	                $response['options_url'] = $url;
+	                $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
+	                return;
+	            }
+
 				$related = $this->getRequest()->getParam('related_product');
 
 				/**
@@ -65,6 +110,10 @@ class Bluecom_Magento1_CartController extends Mage_Checkout_CartController
 	                    $response['minicart'] = $minicart;
 	                }
 				}
+
+				//Create choice block
+                Mage::register('current_product', $product);
+                $response['productinfo'] = $this->getLayout()->createBlock('core/template')->setTemplate('magento1/catalog/product/view/ajaxcart.phtml')->toHtml();
 			} catch (Mage_Core_Exception $e) {
 				$msg = "";
 				if ($this->_getSession()->getUseNotice(true)) {
@@ -89,6 +138,38 @@ class Bluecom_Magento1_CartController extends Mage_Checkout_CartController
 			return parent::addAction();
 		}
 	}
+
+	/**
+     * Get product infomation
+     *
+     * @author Thanh-TV
+     * @since 2016-07-21
+     */
+	public function optionsAction() {
+        $productId = $this->getRequest()->getParam('product_id');
+        // Prepare helper and params
+        $viewHelper = Mage::helper('catalog/product_view');
+
+        $params = new Varien_Object();
+        $params->setCategoryId(false);
+        $params->setSpecifyOptions(false);
+
+        // Render page
+        try {
+            $viewHelper->prepareAndRender($productId, $this, $params);
+        } catch(Exception $e) {
+            if($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
+                if(isset($_GET['store']) && !$this->getResponse()->isRedirect()) {
+                    $this->_redirect('');
+                } elseif(!$this->getResponse()->isRedirect()) {
+                    $this->_forward('noRoute');
+                }
+            } else {
+                Mage::logException($e);
+                $this->_forward('noRoute');
+            }
+        }
+    }
 
 	/**
      * Create block for rwd template
--- js/bluecom/bluecom_ajax.js
+++ js/bluecom/bluecom_ajax.js
@@ -1,10 +1,45 @@
+// Flash is minacatUpdate
 var minicartUpdate = false;
+var cancelTimeOut = 0;
+// Redirect status
+var redirect_status;
+// Redirect timeout
+var redirect_timeout;
+// Cart url
+var cart_url;
+// Show popup
+var show_pop_up;
+// popup
+var win;
 var ajaxLoader = "<div id='loader' style='position: fixed; text-align: center; z-index: 9999; color: black; overflow: auto; min-height: 50px; display:none; margin: 50px; left: 43%; top: 40%; padding: 25px; width: auto; height: auto; margin-left: auto; margin-right: auto;'>" +
     "<img id='loaderGif' style='margin: auto;' src='/skin/frontend/base/default/images/bluecom/opc-ajax-loader.gif'><p style='font: 16px/1.55 Arial, Helvetica, sans-serif; color: white;'>Please wait...</p></div>" +
     "<div id='acoverlay' style='display:none;position: fixed;top: 0;left: 0;width: 100%;height: 100%;background-color: #000;-moz-opacity: 0.3;opacity: .30;filter: alpha(opacity=30);z-index: 9990;'></div>";
 
 jQuery(document).ready(function () {
     jQuery("body").append(ajaxLoader);
+
+    // Popup show product
+    win = new Window({
+        id: "alphacube",
+        className: "alphacube",
+        title: "Please, choose product options",
+        minWidth: 398,
+        minHeight: 490,
+        maxHeight: 1200,
+        maxWidth: 1200,
+        closable: true,
+        minimizable: false,
+        maximizable: false,
+        resizable: false,
+        draggable: false,
+        onClose: closeAjaxCart,
+        showEffect: Element.show,
+        hideEffect: Element.hide
+    });
+
+    // 2016-07-21 Thanh-TV: change view button to cart button
+    jQuery(".category-products a.button").prop("title", "Add to Cart");
+    jQuery(".category-products a.button").text("Add to Cart");
 
     //for theme rwd
     // Rebind event click on minicart after load ajax.
@@ -32,6 +67,15 @@ jQuery(document).ready(function () {
     jQuery(document).on('click', ".category-products .btn-cart", function () {
         addToCart(this.value);
     });
+
+    // 2016-07-21 Thanh-TV: view button (change on add to cart label)
+    jQuery(document).on('click', ".category-products a.button", function (event) {
+        event.preventDefault();
+        addToCart(this.href);
+    });
+
+    //fix bug top window
+    jQuery("#alphacube_row1").removeClass('top');
 });
 
 /**
@@ -74,6 +118,10 @@ function addToCart(url) {
     });
 };
 
+function getBaseUrl() {
+    return document.location.origin;
+}
+
 function showLoading() {
     jQuery('#acoverlay').show();
     jQuery('#loader').show();
@@ -85,12 +133,11 @@ function hideLoading() {
 };
 
 function setAjaxData(data) {
-    /*//fill all global variables
+    //fill all global variables
     redirect_status = data.redirect_status;
     redirect_timeout = data.redirect_timeout;
     cart_url = data.cart_url;
     show_pop_up = data.show_pop_up;
-
     if (data.status == 'ERROR') {
         alert(data.message);
     } else {
@@ -110,44 +157,143 @@ function setAjaxData(data) {
                 selector_iframe.attr('scrolling', 'no');
             }
         }
-        //if show product info enable
+        /*//if show product info enable
         if (data.productinfo) {
             if (jQuery('#choice').length) {
                 jQuery('#choice').remove();
             }
             jQuery('.col-main').append(data.productinfo);
             ajaxshow();
-    }*/
-    //update top links for cart
-    if (data.toplink) {
-        jQuery('.header .links').replaceWith(data.toplink);
-    }
-    //update sidebar cart
-    if (data.sidebar) {
-        if (jQuery('.block-cart')) {
-            jQuery('.block-cart').replaceWith(data.sidebar);
+        }*/
+        //update top links for cart
+        if (data.toplink) {
+            jQuery('.header .links').replaceWith(data.toplink);
         }
-        // ajaxshow();
-    }
-    //update minicart for rwd theme
-    if (data.minicart) {
-        if (jQuery('.header-minicart')) {
-            jQuery('.header-minicart').empty().append(data.minicart);
+        //update sidebar cart
+        if (data.sidebar) {
+            if (jQuery('.block-cart')) {
+                jQuery('.block-cart').replaceWith(data.sidebar);
+            }
+            ajaxshow();
         }
-        // ajaxshow();
-    }
-    //update cart
-    if (data.checkout) {
-        if (jQuery('.cart').length) {
-            jQuery('.cart').replaceWith(data.checkout);
+        //update minicart for rwd theme
+        if (data.minicart) {
+            if (jQuery('.header-minicart')) {
+                jQuery('.header-minicart').empty().append(data.minicart);
+            }
+            ajaxshow();
         }
-        ajaxshow();
-        jQuery(".button.btn-update").remove();
-        jQuery(".button2.btn-update").remove();
+        //update cart
+        if (data.checkout) {
+            if (jQuery('.cart').length) {
+                jQuery('.cart').replaceWith(data.checkout);
+            }
+            ajaxshow();
+            jQuery(".button.btn-update").remove();
+            jQuery(".button2.btn-update").remove();
+        }
+        /*//if response contains wishlist block
+        if (data.wishlist) {
+            updateWishlist(data);
+        }*/
+        minicartUpdate = true;
     }
-    /*//if response contains wishlist block
-    if (data.wishlist) {
-        updateWishlist(data);
-    }*/
-    minicartUpdate = true;
-}
+};
+
+/**
+ * Auto close popup options
+ *
+ * @author Thanh-TV
+ * @since 2016-07-21
+**/
+function respondToClick() {
+    cancelTimeOut = 1;
+    if (!redirect_timeout > 0) {
+        cancelTimeOut = 1;
+    }
+    closeAjaxCart();
+    win.close();
+};
+
+/**
+ * Close popup
+ *
+ * @author Thanh-TV
+ * @since 2016-07-21
+**/
+function closeAjaxCart() {
+    cancelTimeOut = 1;
+    jQuery('#ajaxcart').hide();
+    hideLoading();
+    win.setHTMLContent("");
+};
+
+/**
+ * Load popup options of product
+ *
+ * @author Thanh-TV
+ * @since 2016-07-21
+**/
+function ajaxCartLoad() {
+    var frame = document.getElementById(win.getId());
+    frame.style.zIndex = "9995";
+    win.showCenter();
+    Event.observe("#acoverlay", 'click', respondToClick);
+    hideLoading();
+};
+
+function ajaxshow() {
+    if (redirect_status == 1) {
+        win.hide();
+        var acoverlay = document.getElementById("acoverlay");
+        if (redirect_timeout > 0) {
+            showChoice();
+        } else {
+            jQuery('#loader').show();
+            window.location.replace(cart_url);
+        }
+    } else {
+        win.hide();
+        showChoice();
+    }
+};
+
+function showChoice() {
+    if (show_pop_up > 0) {
+        cancelTimeOut = 0;
+        Event.observe("acoverlay", 'click', respondToClick);
+        jQuery('#loader').hide();
+        jQuery('#choice').show();
+        jQuery('#acoverlay').show();
+        if (redirect_timeout > 0) {
+            countdown(redirect_timeout);
+        }
+    } else {
+        closeAjaxCart();
+    }
+};
+
+function countdown(seconds) {
+    function tick() {
+        var counter = document.getElementById("timeout");
+        if (counter) {
+            counter.innerHTML = String(seconds);
+            if (seconds > 0) {
+                seconds--;
+                if (cancelTimeOut == 0) {
+                    setTimeout(tick, 1000);
+                }
+            } else {
+                if (cancelTimeOut == 0) {
+                    showLoading();
+                    jQuery('#choice').hide();
+                    window.location.replace(cart_url);
+                }
+            }
+        }
+    }
+
+    if (cancelTimeOut == 0 || seconds > 0) {
+        tick();
+    }
+};
